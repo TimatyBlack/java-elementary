317fbca1704c6e6789713be9b635465c99cba865
chugunov75 <b.chugunov75@gmail.com>
2019-11-23 16:09:29 +0200

    Add new lesson

---
+++ src/com/company/partAbstraction/lesson6/LinkedListTest.java
@@ -0,0 +1,13 @@
+package com.company.partAbstraction.lesson6;
+import java.util.List;
+import java.util.LinkedList;
+public class LinkedListTest {
+    public static void main(String[] args) {
+        LinkedList <String> list = new LinkedList<>();
+
+        list.add("Test");
+        list.remove(0);
+        list.addFirst("Test123");
+
+    }
+}
